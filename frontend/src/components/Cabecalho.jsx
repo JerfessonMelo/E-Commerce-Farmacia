fab2759fa71ecb6ac50b33188abb79511de36d20
JerfessonMelo <jerfessonmelo123@gmail.com>
2025-06-14 16:36:56 -0300

    correção do caminho style home.css

--- frontend/src/components/Cabecalho.jsx
+++ frontend/src/components/Cabecalho.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { obterDadosUsuario, removerToken } from "../services/auth";
-import "../styles/Header.css";
+import "../styles/Home.css";
 
 const Cabecalho = () => {
   const navigate = useNavigate();
