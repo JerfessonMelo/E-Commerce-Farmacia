c680199b01bb2b103caa49caa9afc4b1d2f5e7c7
JerfessonMelo <jerfessonmelo123@gmail.com>
2025-06-14 15:37:15 -0300

    modificação de caminho de obter token corrigido

--- frontend/src/pages/painelAdmin/Dashboard.jsx
+++ frontend/src/pages/painelAdmin/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { obterToken } from "../services/auth";
+import { obterToken } from "../../services/api";
 import api from "../../services/api";
 import "../../styles/Dashboard.css";
 
