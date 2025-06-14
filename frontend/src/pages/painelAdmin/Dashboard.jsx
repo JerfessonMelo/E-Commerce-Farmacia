1df32964381654cf0e028d079b155074feb229be
JerfessonMelo <jerfessonmelo123@gmail.com>
2025-06-14 15:39:11 -0300

    erro meu fazer correção para a api, era para ser para o auth hehe

--- frontend/src/pages/painelAdmin/Dashboard.jsx
+++ frontend/src/pages/painelAdmin/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { obterToken } from "../../services/api";
+import { obterToken } from "../../services/auth";
 import api from "../../services/api";
 import "../../styles/Dashboard.css";
 
